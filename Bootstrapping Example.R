836c066c90bdf42d8b304b09d546070d2f2f49d1
Angelo <83621458+AngeloPStats@users.noreply.github.com>
2024-04-08 20:57:23 -0700

    Add files via upload

---
+++ Bootstrapping Example.R
@@ -0,0 +1,62 @@
+### title: "Bootstraping_Example"
+### author: "AngeloPStats"
+### date: "2022-03-20"
+
+#This dataset is available in R#
+
+data("attenu")
+
+dim(attenu)
+
+attach(attenu)
+
+head(attenu)
+
+## Original dataset
+
+lm0 = lm(accel ~ dist + mag , data = attenu)
+summary(lm0)
+plot(lm0)
+
+# Replicating 1000 time the dataset 
+
+index = seq(1,182,1)
+index
+
+boot.out=NULL
+
+
+for(j in 1:1000)
+{
+  index.new = sample(index,182,replace=T)
+  
+  atte.new = attenu[index.new,]
+  
+  lm1 =lm(accel ~ dist + mag, data = atte.new)
+  
+  ##Collects coefficients
+  
+  boot.out=rbind(boot.out,coef(lm1))
+}
+
+
+
+##Plotting 
+
+par(mfrow=c(3,1))
+
+hist(boot.out[,1])
+hist(boot.out[,2])
+hist(boot.out[,3])
+
+
+bt1=mean(boot.out[,1])
+bt2=mean(boot.out[,2])
+bt3=mean(boot.out[,3])
+
+print(c(bt1,bt2,bt3))
+
+##Original Coefficients
+
+coef(lm0)
+
